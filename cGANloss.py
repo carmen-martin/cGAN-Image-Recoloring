a9eb14e936030ddb1938a9ee54d5486ba6d713ce
carmen-martin <cmartur@gmail.com>
2023-01-26 19:39:33 +0100

    Loss updated

--- cGANloss.py
+++ cGANloss.py
@@ -3,19 +3,22 @@ import torch.nn as nn
 
 
 class CGANDiscriminatorLoss(nn.Module):
-    def __init__(self):
-        pass
+    def __init__(self, device, smoothing=0.05):
+        self.device = device
+        self.smoothing = smoothing
 
     def __call__(self, real, fake):
-        # Possibility: compare final matrix to a block 1 for real, and a block 0 for fake instead of mean
-        loss = -1 * torch.mean(torch.log(real) + torch.log(1 - fake))
+        loss = 0.5 * (nn.BCELoss()(fake, torch.zeros(fake.size()).to(device=self.device)) +\
+                      nn.BCELoss()(real, torch.ones(real.size()).to(device=self.device) - self.smoothing)) # One-sided smoothing
         return loss
 
 
 class CGANGeneratorLoss(nn.Module):
-    def __init__(self):
-        pass
+    def __init__(self, device, reg):
+        self.device = device
+        self.reg = reg
 
-    def __call__(self, fake):
-        loss = -1 * torch.mean(torch.log(fake))  # Trick
+    def __call__(self, fake, colored, g_output):
+        loss = -1 * nn.BCELoss()(fake, torch.ones(fake.size()).to(device=self.device)) +\
+               self.reg * nn.L1Loss()(colored, g_output) # Training Trick
         return loss

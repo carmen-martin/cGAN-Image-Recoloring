2a5472bf7fa592b40e4b368db9b89533a8978a6d
carmen-martin <cmartur@gmail.com>
2023-01-26 19:37:38 +0100

    updated generator, discriminator

--- PatchGAN.py
+++ PatchGAN.py
@@ -15,26 +15,26 @@ class PatchGANConvBlock(nn.Module):
             if module.bias is not None:
                 module.bias.data.zero_()
 
-    def forward(self, input):
-        out = self.norm(self.conv(input))
-        out = f.relu(out)
+    def forward(self, x):
+        out = self.norm(self.conv(x))
+        out = nn.LeakyReLU(0.2, True)(out)
         #print(f'ConvBlock {out.size() = }')
         return out
 
 
 class PatchGAN(nn.Module):
-    def __init__(self, patch_size=64, kernel_size=4, stride=4, padding=1):
+    def __init__(self, color_channels=3, patch_size=64, kernel_size=4, stride=2, padding=1):
         super(PatchGAN, self).__init__()
         # Find layers to have a receptive field of 64 x 64 for each output element, given kernel_size, stride, padding
         # R(k) = R(k-1) + 3 * 4 ** (k - 1), Receptive Field for layer k, given kernel size 4 and stride 4.
         # R(k) = R(k - 1) + (kernel_size(k) - 1) * ∏_i^(k-1) stride(i), more generally
-        n_layers = 3
-        nf = 4
+        nf = [color_channels + 1, 64, 128, 256, 512, 1]
+        n_layers = len(nf) - 2
         layers = []
         for n in range(n_layers - 1):
-            nf *= n + 1
-            layers += [PatchGANConvBlock((nf, max(nf * n, 8)), kernel_size=kernel_size, stride=stride, padding=padding)]
-        layers += [nn.Conv2d(max(nf * n, 8), 1, kernel_size=kernel_size, stride=stride, padding=padding)]
+            layers += [PatchGANConvBlock((nf[n], nf[n + 1]), kernel_size=kernel_size, stride=stride, padding=padding)]
+        layers += [PatchGANConvBlock((nf[-3], nf[-2]), kernel_size=kernel_size, stride=1, padding=padding)]
+        layers += [nn.Conv2d(nf[-2], nf[-1], kernel_size=kernel_size, stride=1, padding=padding)]
         self.model = nn.Sequential(*layers)
 
     def _init_weights(self, module):
--- UNet.py
+++ UNet.py
@@ -4,45 +4,22 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torchvision.transforms.functional import crop
 
-# Double 3x3 convolution (convolution-BatchNorm-activation) 
-class double_convolution(nn.Module):
-    
-    def __init__(self, in_channels, out_channels):
-        super().__init__()
-        self.core_layers = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding='valid'),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(),
-            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding='valid'),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU()
-        )
-        
-        self.apply(self._init_weights)
-
-    def _init_weights(self, module):
-        if isinstance(module, torch.nn.Linear):
-            torch.nn.init.xavier_uniform_(module.weight)
-            if module.bias is not None:
-                module.bias.data.zero_()
-        if isinstance(module, torch.nn.Conv2d):
-            torch.nn.init.xavier_uniform_(module.weight)
-            if module.bias is not None:
-                module.bias.data.zero_()
-
-    def forward(self, x):
-        return self.core_layers(x)
-
-
-# Downsampling (doubleconv+pooling)
+# Downsampling
 class downsampling(nn.Module):
     
-    def __init__(self, in_channels, out_channels):
+    def __init__(self, in_channels, out_channels, activation=True, normalization=True):
         super().__init__()
-        self.downsample = nn.Sequential(
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            double_convolution(in_channels, out_channels)
-        )
+        
+        self.activation = activation
+        self.normalization = normalization
+        
+        self.downsample = nn.Conv2d(in_channels, out_channels, kernel_size=4, stride=2, padding=1)
+        
+        if activation:
+            self.act = nn.LeakyReLU(0.2)
+        if normalization:
+            self.norm = nn.BatchNorm2d(out_channels)
+        
         self.apply(self._init_weights)
     
     def _init_weights(self, module):
@@ -56,23 +33,34 @@ class downsampling(nn.Module):
                 module.bias.data.zero_()
 
     def forward(self, x):
-        return self.downsample(x)
+        x = self.downsample(x)
+        
+        if self.normalization:
+            x = self.norm(x)
+        if self.activation:
+            x = self.act(x)
+        
+        return x
 
 
 # Upsampling (doubleconv+upsampling)
 class upsampling(nn.Module):
     
-    def __init__(self, in_channels, out_channels, upsampling_mode, concat_mode):
+    def __init__(self, in_channels, out_channels, activation=True, normalization=True, dropout=False):
         super().__init__()
-        self.upsamplig_mode = upsampling_mode
-        self.concat_mode = concat_mode
+        self.activation = activation
+        self.normalization = normalization
+        self.dropout = dropout
         
-        self.upsample = nn.Sequential(
-            nn.Upsample(scale_factor=2, mode=upsampling_mode), #mode can be:'nearest','linear','bilinear','bicubic','trilinear'
-            nn.Conv2d(in_channels, in_channels//2, kernel_size=1)
-        ) #Another possibility for upsampling is using an inverse conv with nn.ConvTranspose2d
-        self.double_conv = double_convolution(in_channels, out_channels)
-
+        self.upsample = nn.ConvTranspose2d(in_channels, out_channels, kernel_size=4, stride=2, padding=1)
+        
+        if activation:
+            self.act = nn.ReLU()
+        if normalization:
+            self.norm = nn.BatchNorm2d(out_channels)
+        if dropout:
+            self.drop = nn.Dropout2d(p=0.5)
+        
         self.apply(self._init_weights)
     
     def _init_weights(self, module):
@@ -85,45 +73,47 @@ class upsampling(nn.Module):
             if module.bias is not None:
                 module.bias.data.zero_()
 
-    def forward(self, x, x_down):   
+    def forward(self, x, x_down):
+        if self.activation:
+            x = self.act(x)
+            
         x = self.upsample(x)
         
-        if self.concat_mode == 'different':
-            # Crop contracting path outputs and concat with expanding map
-            H = x.size(dim=2)
-            W = x.size(dim=3)  
-            top = (x_down.size(dim=2) - H)//2
-            left = (x_down.size(dim=3) - W)//2
-
-            cropped_x_down = crop(x_down, top, left, H, W)
-            x = cat((x, cropped_x_down), dim=1) #check dim
-            
-        elif self.concat_mode == 'same':
+        if self.normalization:
+            x = self.norm(x)
+        if self.dropout:
+            x = self.drop(x)
+        
+        H_x = x.size(dim=2)
+        W_x = x.size(dim=3)
+        H_xd = x_down.size(dim=2)
+        W_xd = x_down.size(dim=3)
+        
+        if H_x == H_xd and W_x == W_xd:
+            x = cat((x, x_down), dim=1) #check if this is possible with dims or we need to crop/pad
+        else:
             # Pad expanding path outputs and concat with contracting map
-            H_diff = x_down.size(dim=2) - x.size(dim=2)
+            H_diff = H_xd - H_x
             H_left = H_diff//2
-            W_diff = x_down.size(dim=3) - x.size(dim=3)
+            W_diff = W_xd - W_x
             W_left = W_diff//2
             padded_x = F.pad(x, (W_left, (W_diff - W_left), H_left, (H_diff - H_left)))
             x = cat((padded_x, x_down), dim=1)
-        else:
-            raise Exception(f"{self.concat_mode} is not a valid input for output_input")
         
-        return self.double_conv(x)
+        return x
     
 # Final layer (class mapping)
 class final(nn.Module):
     
-    def __init__(self, in_channels, n_outputs, output_size, upsampling_mode, output_input):
+    def __init__(self, in_channels, n_outputs):
         super().__init__()
-        self.output_input = output_input
         
-        self.output_same = nn.Sequential(
-            nn.Upsample(size=output_size, mode=upsampling_mode),
-            nn.Conv2d(in_channels, n_outputs, kernel_size=1)
+        self.output = nn.Sequential(
+            nn.ReLU(),
+            nn.ConvTranspose2d(in_channels, n_outputs, kernel_size=4, stride=2, padding=1),
+            nn.Sigmoid()
         )
         
-        self.output_valid = nn.Conv2d(in_channels, n_outputs, kernel_size=1)
         self.apply(self._init_weights)
 
     def _init_weights(self, module):
@@ -137,58 +127,75 @@ class final(nn.Module):
                 module.bias.data.zero_()
 
     def forward(self, x):
-        # For output_size = input_size
-        if self.output_input == 'same':
-            return self.output_same(x)
-        # If it is okay for output and input sizes being different
-        elif self.output_input == 'different':
-            return self.output_valid(x) 
-        else:
-            raise Exception(f"{self.output_input} is not a valid input for output_input")
+        return self.output(x)
 
 # UNet
 class UNet(nn.Module):
-    def __init__(self, output_size, in_channels=1, out_channels=3, upsampling_mode='nearest', output_input='same'):
+    def __init__(self, in_channels=1, out_channels=3):
         super().__init__()
         self.in_channels = in_channels
         self.out_channels = out_channels
         
         # Input process
-        self.input_process = double_convolution(in_channels, 64)
+        self.input_process = nn.Conv2d(in_channels, 64, kernel_size=4, stride=2, padding=1)
         
         # Contracting path - double feature channels at each downsampling
-        self.contraction1 = downsampling(64, 128)
+        self.contraction1 = downsampling(64, 128, normalization=False)
         self.contraction2 = downsampling(128, 256)
         self.contraction3 = downsampling(256, 512)
-        self.contraction4 = downsampling(512, 1024)
+        self.contraction4 = downsampling(512, 512)
+        self.contraction5 = downsampling(512, 512)
+        self.contraction6 = downsampling(512, 512)
+        self.contraction7 = downsampling(512, 512)
+        
+        self.mid_process = nn.Conv2d(512, 512, kernel_size=3, stride=2, padding=1)
         
         # Expanding path - half features channels at each upsampling
-        self.expansion1 = upsampling(1024, 512, upsampling_mode=upsampling_mode, concat_mode=output_input)
-        self.expansion2 = upsampling(512, 256, upsampling_mode=upsampling_mode, concat_mode=output_input)
-        self.expansion3 = upsampling(256, 128, upsampling_mode=upsampling_mode, concat_mode=output_input)
-        self.expansion4 = upsampling(128, 64, upsampling_mode=upsampling_mode, concat_mode=output_input)
+        self.expansion1 = upsampling(512, 512, dropout=True)
+        self.expansion2 = upsampling(1024, 512, dropout=True)
+        self.expansion3 = upsampling(1024, 512, dropout=True)
+        self.expansion4 = upsampling(1024, 512)
+        self.expansion5 = upsampling(1024, 256)
+        self.expansion6 = upsampling(512, 128)
+        self.expansion7 = upsampling(256, 64)
         
         # Output process
-        self.output = final(64, out_channels, output_size=output_size, upsampling_mode=upsampling_mode, output_input=output_input)
-        
-        # Dropout
-        self.init_dropout = nn.Dropout2d(p=0.2)
-        self.final_dropout = nn.Dropout2d(p=0.2)
+        self.output = final(128, out_channels)
     
     def forward(self, x):
         x_init = self.input_process(x)
-        # Dropout as noise
-        x_init = self.init_dropout(x_init)
+        print(f'{x_init.shape = }')
         x_down1 = self.contraction1(x_init)
+        print(f'{x_down1.shape = }')
         x_down2 = self.contraction2(x_down1)
+        print(f'{x_down2.shape = }')
         x_down3 = self.contraction3(x_down2)
+        print(f'{x_down3.shape = }')
         x_down4 = self.contraction4(x_down3)
-        x = self.expansion1(x_down4, x_down3)
-        x = self.expansion2(x, x_down2)
-        x = self.expansion3(x, x_down1)
-        x = self.expansion4(x, x_init)
-        # Dropout as noise
-        #x = self.final_dropout(x)
+        print(f'{x_down4.shape = }')
+        x_down5 = self.contraction5(x_down4)
+        print(f'{x_down5.shape = }')
+        x_down6 = self.contraction6(x_down5)
+        print(f'{x_down6.shape = }')
+        x_down7 = self.contraction7(x_down6)
+        print(f'{x_down7.shape = }')
+        x_mid = self.mid_process(x_down7)
+        print(f'{x_mid.shape = }')
+        x = self.expansion1(x_mid, x_down6)
+        print(f'{x.shape = }')
+        x = self.expansion2(x, x_down5)
+        print(f'{x.shape = }')
+        x = self.expansion3(x, x_down4)
+        print(f'{x.shape = }')
+        x = self.expansion4(x, x_down3)
+        print(f'{x.shape = }')
+        x = self.expansion5(x, x_down2)
+        print(f'{x.shape = }')
+        x = self.expansion6(x, x_down1)
+        print(f'{x.shape = }')
+        x = self.expansion7(x, x_init)
+        print(f'{x.shape = }')
+        print(f'{self.output(x).shape = }')
         return self.output(x)
     
     def _init_weights(self, module):
